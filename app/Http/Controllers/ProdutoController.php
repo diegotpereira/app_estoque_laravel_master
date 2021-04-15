901562e573c2e95fadf6ad7084d89e1549da0ce1
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-04-15 10:56:50 -0300

    criação do metodo pesquisar.

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    //
+    public function pesquisar()
+    {
+        $produtos = Produto::all();
+
+        foreach ($produtos as $produto) {
+            # code...
+            echo $produto->descricao . "<br/>";
+        }
+    }
+}
