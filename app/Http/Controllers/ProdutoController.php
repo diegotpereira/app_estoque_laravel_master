a2040b6bf7e754e6f94e058ef68817f89480b818
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-04-15 17:25:17 -0300

    alteração nos arquivos php para utilização metodo inserir.

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,18 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produto;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+
 
 class ProdutoController extends Controller
 {
     //
     public function pesquisar()
     {
-        $produtos = Produto::all();
+        $descricao = Input::get('descricao');
 
-        foreach ($produtos as $produto) {
-            # code...
-            echo $produto->descricao . "<br/>";
-        }
+        $produtos = Produto::where('descricao', 'like', '%'.$descricao.'%')->get();
+
+        return view('produto.pesquisar')->with('produtos', $produtos);
+    }
+    // metodo inserir na tabela
+    public function mostrar_inserir()
+    {
+        return view('produto.inserir');
+    }
+    // função inserir produto
+    public function inserir(Request $request)
+    {
+        $produto = new Produto();
+
+        $produto->descricao = Input::get('descricao');
+        $produto->quantidade = Input::get('quantidade');
+        $produto->valor = Input::get('valor');
+        $produto->data_vencimento = Input::get('data_vencimento');
+
+        $produto->save();
+
+        $mensagem = "Produto inserido com sucesso";
+
+        return view('produto.inserir')->with('mensagem', $mensagem);
     }
 }
---
+++ resources/views/produto/inserir.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimu-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Inserir Produto</title>
+
+     <link href="../../css/app.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1 class="mt-2">Inserir Produto</h1>
+        @if(!empty($mensagem))
+            <div class="alert alert-success">Produto Inserido com Sucesso!.</div>
+        @endif
+        <form action="/produtos/inserir" method="post" class="mt-2">
+            <input type="hidden" name="_token" value="{{{ csrf_token() }}}">
+
+            <div class="form-group">
+                <label for="descricao">Descrição: <span class="text-danger">*</span></label>
+                <input type="text" id="descricao" name="descricao" class="form-control" autofocus required>
+            </div>
+
+            <div class="form-group">
+                <label for="quantidade">Quantidade: <span class="text-danger">*</span></label></label>
+                <input type="number" id="quantidade" name="quantidade" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="valor">Valor: <span class="text-danger">*</span></label></label>
+                <input type="number" id="valor" name="valor" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+               <label for="data_vencimento">Data de Vencimento: <span class="text-danger">*</span></label></label>
+               <input type="date" id="data_vencimento" name="data_vencimento" class="form-control">
+            </div>
+
+            <div>Os campos marcados com <span class="text-danger">*</span> não podem estar em branco</div>
+
+            <input type="submit" class="btn btn-success mt-2" value="Inserir">
+        </form>
+    </div>
+</body>
+</head>
+</html>
